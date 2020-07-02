dad3192a336d14a37b62594eff05eb22ba04e1af
Wojciech Knott <wojciechknott@gmail.com>
2020-07-02 17:20:45 +0200

    added buttons to tasks list

--- js/script.js
+++ js/script.js
@@ -17,13 +17,35 @@
     addTask(newTaskInput);
   };
 
+  const removeTask = (index) => {
+    tasks.splice(index, 1);
+    render();
+  };
+
+  const toggleDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
   const render = () => {
     let htmlString = "";
     for (const task of tasks) {
-      htmlString += `<li> ${task.name} </li>
-`;
+      htmlString += `<li> 
+      <button class="js-done">${task.done ? "✔️" : "Zrobione"}</button>
+      ${task.done ? `<s>${task.name}</s>` : `${task.name}`}
+      <button class="js-remove">Usuń</button>
+      </li>  
+      `;
     }
     document.querySelector(".js-tasksList").innerHTML = htmlString;
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, taskIndex) => {
+      removeButton.addEventListener("click", () => removeTask(taskIndex));
+    });
+    const doneButtons = document.querySelectorAll(".js-done");
+    doneButtons.forEach((doneButton, taskIndex) => {
+      doneButton.addEventListener("click", () => toggleDone(taskIndex));
+    });
   };
   const init = () => {
     document.querySelector(".js-newTask").focus();
