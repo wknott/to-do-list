ad8f026edb1b5f2ab15163efbbcd8f811e4cdb9e
Wojciech Knott <wojciechknott@gmail.com>
2020-07-02 16:20:53 +0200

    added init and onFormSubmit functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 {
-  console.log("test");
+  const tasks = [];
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    const newTaskInput = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskInput.value.trim();
+    if (!newTaskContent) {
+      newTaskInput.focus();
+      return;
+    }
+    tasks.push(newTaskContent);
+    newTaskInput.value = "";
+    newTaskInput.focus();
+  };
+
+  const init = () => {
+    document.querySelector(".js-newTask").focus();
+    document.querySelector(".js-form").addEventListener("submit", onFormSubmit);
+  };
+  init();
 }
